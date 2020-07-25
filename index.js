b2fbde13937f6cb4198c1698906246af1d27ed82
eli <eligoodale@yahoo.com>
2020-07-25 16:04:04 -0600

    Added index.js, managing secrets with dotenv, ready for test

---
+++ index.js
@@ -0,0 +1,18 @@
+// use env secrets
+const env = require('dotenv');
+env.config();
+
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(process.env.CLIENT_KEY);
