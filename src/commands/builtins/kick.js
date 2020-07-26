f0c471af59994fe933526e4600d556bbbf207f57
eli <eligoodale@yahoo.com>
2020-07-26 05:20:42 -0600

    changed eslint rule no-unused-vars off

--- src/commands/builtins/kick.js
+++ src/commands/builtins/kick.js
@@ -7,9 +7,7 @@ const kick = new Command(
     if (!message.mentions.users.size) {
       return message.reply('you need to tag a user in order to kick them!');
     }
-
     const taggedUser = message.mentions.users.first();
-
     message.channel.send(`You wanted to kick: ${taggedUser.username}`);
   });
 
